550cf79ec14ae7638b7c936376e852cac8d118ec
Jean Giraldo <jeann.giraldo@gmail.com>
2025-08-27 19:58:11 -0500

    refactor(neovim): reword window keymap descriptions for clarity

--- nvim/lua/config/keymaps/window_management.lua
+++ nvim/lua/config/keymaps/window_management.lua
@@ -1,50 +1,44 @@
 return {
 	{
-		desc = "Move to window left",
-		mode = "n",
-		keys = "<leader>wh",
-		cmd = "<C-w>h",
-	},
-	{
-		desc = "Move to window below",
-		mode = "n",
-		keys = "<leader>wj",
-		cmd = "<C-w>j",
-	},
-	{
-		desc = "Move to window above",
-		mode = "n",
-		keys = "<leader>wk",
-		cmd = "<C-w>k",
-	},
-	{
-		desc = "Move to window right",
-		mode = "n",
-		keys = "<leader>wl",
-		cmd = "<C-w>l",
-	},
-	{
 		desc = "Vertical split",
 		mode = "n",
 		keys = "<leader>ws",
 		cmd = ":vsplit <CR>",
 	},
 	{
-		desc = "Vertical split with Fuzzy Finder",
-		mode = "n",
-		keys = "<leader>sV",
-		cmd = ":vsplit<CR>:normal ;ff<CR>",
-	},
-	{
-		desc = "Increase VSplit",
+		desc = "Increase vertical split",
 		mode = "n",
 		keys = "<C-Up>",
 		cmd = ":vertical resize +3 <CR>",
 	},
 	{
-		desc = "Decrease VSplit",
+		desc = "Decrease vertical split",
 		mode = "n",
 		keys = "<C-Down>",
 		cmd = ":vertical resize -3 <CR>",
+	},
+	{
+		desc = "Move to left split",
+		mode = "n",
+		keys = "<leader>wh",
+		cmd = "<C-w>h",
+	},
+	{
+		desc = "Move to split below",
+		mode = "n",
+		keys = "<leader>wj",
+		cmd = "<C-w>j",
+	},
+	{
+		desc = "Move to split above",
+		mode = "n",
+		keys = "<leader>wk",
+		cmd = "<C-w>k",
+	},
+	{
+		desc = "Move to right split",
+		mode = "n",
+		keys = "<leader>wl",
+		cmd = "<C-w>l",
 	},
 }
