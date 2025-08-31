893d6bf40a0531725f70dacca4228663ff430410
Jean Giraldo <jeann.giraldo@gmail.com>
2025-08-31 18:52:01 -0500

    refactor(neovim): move general LSP configuration to lua/lsp.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,18 +11,6 @@ require("highlights")
 
 vim.cmd("colorscheme tokyonight")
 
-vim.lsp.enable({
-	"lua_ls",
-	"rust_analyzer",
-	"basedpyright",
-	"harper_ls",
-	"marksman",
-	"tinymist",
-	"yaml_ls",
-})
-
-vim.lsp.set_log_level(vim.log.levels.OFF)
-vim.lsp.log.set_format_func(vim.inspect)
 vim.diagnostic.config({
 	virtual_text = true,
 })
--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,3 +1,16 @@
+vim.lsp.log.set_level("off")
+vim.lsp.log.set_format_func(vim.inspect)
+
+vim.lsp.enable({
+	"lua_ls",
+	"rust_analyzer",
+	"basedpyright",
+	"harper_ls",
+	"marksman",
+	"tinymist",
+	"yaml_ls",
+})
+
 -- Nerd Font icons for each LSP completion item type
 vim.lsp.protocol.CompletionItemKind = {
 	"󰉿 Text",
