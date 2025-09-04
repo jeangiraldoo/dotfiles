905d73caa09ac83c9d7a918d7be92e8efbf1c072
Jean Giraldo <jeann.giraldo@gmail.com>
2025-09-04 14:33:55 -0500

    refactor(neovim): allow keymap spec opts to override defaults

--- nvim/lua/keymaps/init.lua
+++ nvim/lua/keymaps/init.lua
@@ -1,12 +1,17 @@
 local utils = require("utils")
 local keymaps = utils.get_combined_module_tbls("keymaps")
 
-local opts = {
+local default_opts = {
 	noremap = true,
 	silent = true,
 }
 
 for _, keymap in ipairs(keymaps) do
-	opts.desc = keymap.desc or nil
+	local opts = vim.tbl_extend("force", default_opts, keymap.opts or {})
+
+	if keymap.desc then
+		opts.desc = keymap.desc
+	end
+
 	vim.keymap.set(keymap.mode, keymap.keys, keymap.cmd, opts)
 end
