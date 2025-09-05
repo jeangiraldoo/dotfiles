7a0edd61a4297ebde6817d9853e79d009cd27389
Jean Giraldo <jeann.giraldo@gmail.com>
2025-09-05 16:59:27 -0500

    fix(neovim): move colorscheme to highlights.lua with guard to prevent overrides
    Previously the colorscheme was set in ´init.lua´, which caused custom
    and plugin highlights to be reset whenever ´init.lua´ was reloaded.
    
    The colorscheme setup was moved into ´highlights.lua´ to fix this issue,
    which worked properly for custom highlights but would result in any
    highlights coming from plugins being overriden by the colorscheme, as it
    would be reloaded when the file gets resourced. This is why the
    colorscheme is now only applied if no colorscheme with that name is
    already active, ensuring highlights persist after reloads.

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,14 +2,12 @@ vim.g.mapleader = " "
 
 vim.opt.packpath:prepend(vim.fn.stdpath("data") .. "\\site")
 
-require("opts")
-require("autocommands")
 require("plugins.init")
+require("opts")
+require("highlights")
+require("autocommands")
 require("keymaps")
 require("lsp")
-require("highlights")
-
-vim.cmd("colorscheme tokyonight")
 
 vim.diagnostic.config({
 	virtual_text = true,
--- nvim/lua/highlights.lua
+++ nvim/lua/highlights.lua
@@ -1,3 +1,9 @@
+local COLORSCHEME_NAME = "tokyonight-moon"
+
+if not (vim.g.colors_name == COLORSCHEME_NAME) then
+	vim.cmd("colorscheme " .. COLORSCHEME_NAME)
+end
+
 local HIGHLIGHTS = {
 	WhiteText = {
 		fg = "#ffffff",
