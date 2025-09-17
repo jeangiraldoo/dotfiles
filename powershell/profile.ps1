a48e71837cb36fc35f162d18144c565e7c6ff6aa
Jean Giraldo <jeann.giraldo@gmail.com>
2025-09-17 17:23:24 -0500

    chore: add powershell profile script

---
+++ powershell/profile.ps1
@@ -0,0 +1,22 @@
+# Environment variables
+$env:EDITOR = "nvim"
+$env:STARSHIP_CONFIG = "$HOME/.config/starship.toml"
+
+# Aliases
+Set-Alias e $env:EDITOR
+function G { lazygit @args }
+if (Get-Command eza -ErrorAction SilentlyContinue) {
+    # Remove the built-in ls alias (only for this session)
+    Remove-Item Alias:ls -ErrorAction SilentlyContinue
+
+    function ls {
+        eza --icons=always -a --group-directories-first @args
+    }
+}
+
+# Vi mode
+Set-PSReadLineOption -EditMode vi
+
+# Initializations
+Invoke-Expression (&starship init powershell)
+Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
