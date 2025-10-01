1da7049acc2cb9042b4075d9151cfe6230e70373
Jean Giraldo <jeann.giraldo@gmail.com>
2025-10-01 13:32:46 -0500

    refactor(neovim): move plugin keymaps to their respective plugin spec

--- nvim/lua/keymaps/code_editing.lua
+++ nvim/lua/keymaps/code_editing.lua
@@ -18,34 +18,12 @@ return {
 		cmd = vim.lsp.buf.outgoing_calls,
 	},
 	{
-		desc = "Go to next word occurrence",
-		mode = "n",
-		keys = "<leader>sn",
-		cmd = function()
-			require("illuminate").goto_next_reference(true)
-		end,
-	},
-	{
-		desc = "Go to previous word occurrence",
-		mode = "n",
-		keys = "<leader>sN",
-		cmd = function()
-			require("illuminate").goto_prev_reference(true)
-		end,
-	},
-	{
 		desc = "Toggle inlay hints",
 		mode = "n",
 		keys = "<leader>sh",
 		cmd = function()
 			vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 		end,
-	},
-	{
-		desc = "Insert code annotation",
-		mode = "n",
-		keys = "<leader>sa",
-		cmd = "<plug>Codedocs",
 	},
 	{
 		desc = "Toggle word under the cursor",
@@ -111,20 +89,6 @@ return {
 			end
 
 			vim.cmd.normal("viw~")
-		end,
-	},
-	{
-		desc = "Swap TS nodes",
-		mode = "n",
-		keys = "<leader>ss",
-		cmd = ":ISwapNodeWith<CR>",
-	},
-	{
-		desc = "Go to code context",
-		mode = "n",
-		keys = "<leader>sc",
-		cmd = function()
-			require("treesitter-context").go_to_context(vim.v.count1)
 		end,
 	},
 	{
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -27,6 +27,16 @@ return {
 		src = "https://github.com/nvim-treesitter/nvim-treesitter-context",
 		data = {
 			enabled = true,
+			keymaps = {
+				{
+					desc = "Go to code context",
+					mode = "n",
+					keys = "<leader>sc",
+					cmd = function()
+						require("treesitter-context").go_to_context(vim.v.count1)
+					end,
+				},
+			},
 		},
 	},
 }
