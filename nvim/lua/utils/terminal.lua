7b7f5deb2b52885e2cf82e86f46b46928e1e1271
Jean Giraldo <jeann.giraldo@gmail.com>
2025-10-08 14:46:07 -0500

    refactor(neovim/utils): simplify terminal buffer setup

--- nvim/lua/utils/terminal.lua
+++ nvim/lua/utils/terminal.lua
@@ -1,9 +1,5 @@
 local Terminal = {}
 
-local OS_NAME = vim.loop.os_uname().sysname:lower()
-local DEFAULT_SYSTEM_SHELL_CMD = OS_NAME == "windows_nt" and "powershell -NoLogo" or "$SHELL"
-local CMD_STRUCTURE = "%s ; " .. DEFAULT_SYSTEM_SHELL_CMD
-
 --- Launches a configurable terminal using a vertical split.
 --- @param opts table A table with the following optional settings:
 --- * `cmd`: The shell command to run.
@@ -14,28 +10,22 @@ local CMD_STRUCTURE = "%s ; " .. DEFAULT_SYSTEM_SHELL_CMD
 function Terminal.launch(opts)
 	opts = opts or {}
 
-	local cmd = opts.cmd and CMD_STRUCTURE:format(opts.cmd) or DEFAULT_SYSTEM_SHELL_CMD
-
-	vim.cmd("botright split | resize 15")
-	local term_buf = vim.api.nvim_create_buf(false, true)
-
-	local win = vim.api.nvim_get_current_win()
-	vim.api.nvim_win_set_buf(win, term_buf)
-
-	vim.fn.termopen(cmd, {
-		cwd = opts.cwd or vim.fn.getcwd(),
-	})
-
-	-- Set buffer options to make it behave like a terminal
-	vim.bo[term_buf].buftype = "terminal"
-	vim.bo[term_buf].bufhidden = "hide"
-	vim.bo[term_buf].swapfile = false
+	if opts.close_after_cmd and not opts.cmd then
+		return
+	end
 
 	if opts.close_after_cmd then
-		vim.cmd("close")
-	else
-		vim.cmd("startinsert")
+		vim.fn.jobstart(opts.cmd)
+		return
 	end
+
+	vim.cmd("botright new")
+	vim.cmd.term()
+	local job_id = vim.bo.channel
+
+	-- If opts.cmd is nil, chansend() sends nothing and the shell starts interactively
+	local interactive_terminal_cmd = opts.cmd and opts.cmd .. "\r\n"
+	vim.fn.chansend(job_id, { interactive_terminal_cmd })
 end
 
 return Terminal
