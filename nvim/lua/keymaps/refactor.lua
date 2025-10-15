d5273eca4ec584aa1f1c948289d2f19b5871a76f
Jean Giraldo <jeann.giraldo@gmail.com>
2025-10-15 11:54:00 -0500

    chore(neovim/keymaps): remove unused mappings in favor of built-in motions

--- nvim/lua/keymaps/refactor.lua
+++ nvim/lua/keymaps/refactor.lua
@@ -5,21 +5,4 @@ return {
 		keys = "<leader>rn",
 		cmd = vim.lsp.buf.rename,
 	},
-	{
-		desc = "Empty out string under the cursor",
-		mode = "n",
-		keys = "<leader>rt",
-		cmd = function()
-			local node = vim.treesitter.get_node()
-
-			if node:type() == "string_content" then
-				local bufnr = vim.api.nvim_get_current_buf()
-				local start_row, start_col, end_row, end_col = node:range()
-
-				local empty_string = ""
-
-				vim.api.nvim_buf_set_text(bufnr, start_row, start_col, end_row, end_col, { empty_string })
-			end
-		end,
-	},
 }
--- nvim/lua/keymaps/window_management.lua
+++ nvim/lua/keymaps/window_management.lua
@@ -1,11 +1,5 @@
 return {
 	{
-		desc = "Vertical split",
-		mode = "n",
-		keys = "<leader>ws",
-		cmd = ":vsplit <CR>",
-	},
-	{
 		desc = "Increase vertical split",
 		mode = "n",
 		keys = "<C-Up>",
@@ -16,29 +10,5 @@ return {
 		mode = "n",
 		keys = "<C-Down>",
 		cmd = ":vertical resize -3 <CR>",
-	},
-	{
-		desc = "Move to left split",
-		mode = "n",
-		keys = "<leader>wh",
-		cmd = "<C-w>h",
-	},
-	{
-		desc = "Move to split below",
-		mode = "n",
-		keys = "<leader>wj",
-		cmd = "<C-w>j",
-	},
-	{
-		desc = "Move to split above",
-		mode = "n",
-		keys = "<leader>wk",
-		cmd = "<C-w>k",
-	},
-	{
-		desc = "Move to right split",
-		mode = "n",
-		keys = "<leader>wl",
-		cmd = "<C-w>l",
 	},
 }
