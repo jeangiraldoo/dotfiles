b0ccdeeb44d6db1e7d49e5709d963ce14cf8e4a1
Jean Giraldo <jeann.giraldo@gmail.com>
2025-10-16 23:00:32 -0500

    refactor(nvim/autocommands): centralize autocomplete setup at `code_editing.lua`

--- nvim/lua/autocommands/lsp.lua
+++ nvim/lua/autocommands/lsp.lua
@@ -1,27 +1,5 @@
 return {
 	{
-		desc = "Enable autocompletion",
-		pattern = "*",
-		event = "LspAttach",
-		cmd = function(ev)
-			local client = vim.lsp.get_client_by_id(ev.data.client_id)
-			if client ~= nil and client:supports_method("textDocument/completion") then
-				vim.lsp.completion.enable(true, client.id, ev.buf, {
-					autotrigger = true,
-					convert = function(item)
-						return { abbr = item.label:gsub("%b()", "") }
-					end,
-				})
-			end
-		end,
-	},
-	{
-		desc = "Display autocomplete window while typing",
-		pattern = "*",
-		event = "TextChangedI",
-		cmd = vim.lsp.completion.get,
-	},
-	{
 		desc = "Set up LSP CodeLens",
 		pattern = "*",
 		event = "LspAttach",
