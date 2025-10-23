9eb005cf8896e302e9628e5a475174669e0fd56f
Jean Giraldo <jeann.giraldo@gmail.com>
2025-10-22 19:59:40 -0500

    chore(neovim/autocmds): remove unnecessary function signature help autocommand

--- nvim/plugin/autocmds.lua
+++ nvim/plugin/autocmds.lua
@@ -1,22 +1,6 @@
 local AUTOCMDS = {
 	-- Code editing
 	{
-		desc = "Display function signature help when typing arguments",
-		event = "TextChangedI",
-		pattern = "*",
-		cmd = function()
-			local col = vim.fn.col(".") - 1
-			if col > 0 then
-				local char = vim.fn.getline("."):sub(col, col)
-
-				local is_typing_function_args = (char == "(" or char == ",")
-				if is_typing_function_args then
-					vim.lsp.buf.signature_help()
-				end
-			end
-		end,
-	},
-	{
 		desc = "Lint the current file on save",
 		event = "BufWritePost",
 		pattern = "*",
