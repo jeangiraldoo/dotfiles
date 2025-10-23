2058485b9594ecad28ababe781e3f95f0bfb57b9
Jean Giraldo <jeann.giraldo@gmail.com>
2025-10-23 09:02:55 -0500

    refactor(neovim/interface): move core UI settings to `plugin/`

--- nvim/lua/interface/init.lua
+++
@@ -1,4 +0,0 @@
-require("interface.colorscheme")
-require("interface.highlights")
-require("interface.diagnostics")
-require("interface.signs")
--- nvim/lua/interface/statusline/file/init.lua
+++
@@ -1,4 +0,0 @@
-return {
-	metadata = require("interface.statusline.file.metadata"),
-	status = require("interface.statusline.file.status"),
-}
