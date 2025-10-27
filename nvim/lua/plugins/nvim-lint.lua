af444cf283ab6b81ca21ca8956f6d70342b43df4
Jean Giraldo <jeann.giraldo@gmail.com>
2025-10-26 23:12:36 -0500

    refactor(neovim): move `nvim-lint` autocommand into plugin spec

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -34,6 +34,12 @@ return {
 				"--globals",
 				"vim",
 			}
+
+			vim.api.nvim_create_autocmd("BufWritePost", {
+				callback = function()
+					require("lint").try_lint()
+				end,
+			})
 		end,
 	},
 }
