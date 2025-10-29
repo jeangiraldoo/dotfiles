197aa84ba4b8c5007f12a8d576cbe4551af34f8c
Jean Giraldo <jeann.giraldo@gmail.com>
2025-10-28 23:06:11 -0500

    chore(neovim): set `oil` plugin to display hidden files and skip confirmation

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,8 +1,11 @@
 local OPTS = {
 	default_file_explorer = true,
 	columns = {
-		"permissions",
 		"icon",
+	},
+	skip_confirm_for_simple_edits = true,
+	view_options = {
+		show_hidden = true,
 	},
 }
 
