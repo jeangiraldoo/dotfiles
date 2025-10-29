855318321bb14e471f9f50d2c4471a57e49cf246
Jean Giraldo <jeann.giraldo@gmail.com>
2025-10-29 15:08:27 -0500

    refactor(neovim): move settings from `plugin/` to `lua/` to enable `.nvim.lua` support
    Previously, keymaps, options, autocommands and lsp settings were placed
    at `plugin/` as they would be automatically sourced by Neovim, with no
    change in behaviour.
    
    I recently found out about the `.nvim.lua` file and decided to add
    support for it, but soon realized it was being loaded before the base
    settings, resulting in those overriding the project-specific
    configuration.
    
    This change solves this issue by moving all settings to `lua/`, as it
    gets sourced before the `.nvim.lua` file.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,9 @@
 vim.g.mapleader = " "
+vim.o.exrc = true -- Load project-specific config using `.nvim.lua`
 
-require("plugins.init")
+require("opts")
+require("keymaps")
+require("lsp")
+require("autocmds")
+require("plugins")
+require("ui")
