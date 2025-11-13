eab7a7e8f99610f43df9fbb3acb39ac537e7620f
Jean Giraldo <jeann.giraldo@gmail.com>
2025-11-13 11:22:35 -0500

    chore(wezterm/keymaps): replace unnecessary keymaps with defaults

--- wezterm/keymaps.lua
+++ wezterm/keymaps.lua
@@ -10,50 +10,22 @@ local OPACITY = {
 local is_window_transparent = true
 
 local keymaps = {
-	-- Window transparency keymap
-	{
-		key = "z",
-		mods = "CTRL",
-		action = wezterm.action_callback(function(window, _)
-			is_window_transparent = not is_window_transparent
-
-			local overrides = window:get_config_overrides() or {}
-			overrides.window_background_opacity = is_window_transparent and OPACITY.OPAQUE or OPACITY.TRANSPARENT
-			window:set_config_overrides(overrides)
-		end),
-	},
 	-- Window management
-	{
-		key = "T",
-		mods = "CTRL",
-		action = act.SpawnTab("CurrentPaneDomain"),
-	},
-	{
-		key = "t",
-		mods = "CTRL",
-		action = act.SplitPane({
-			direction = "Right",
-		}),
-	},
 	{
 		key = "q",
 		mods = "CTRL",
 		action = act.CloseCurrentPane({ confirm = true }),
 	},
 	{
-		key = "Q",
+		key = "z",
 		mods = "CTRL",
-		action = act.CloseCurrentTab({ confirm = true }),
-	},
-	{
-		key = "l",
-		mods = "CTRL",
-		action = act.ActivatePaneDirection("Right"),
-	},
-	{
-		key = "h",
-		mods = "CTRL",
-		action = act.ActivatePaneDirection("Left"),
+		action = wezterm.action_callback(function(window, _) -- Toggle transparency
+			is_window_transparent = not is_window_transparent
+
+			local overrides = window:get_config_overrides() or {}
+			overrides.window_background_opacity = is_window_transparent and OPACITY.OPAQUE or OPACITY.TRANSPARENT
+			window:set_config_overrides(overrides)
+		end),
 	},
 	-- Workspaces
 	{
@@ -81,14 +53,5 @@ local keymaps = {
 		end),
 	},
 }
-
--- Tab management
-for i = 0, 9 do
-	table.insert(keymaps, {
-		key = (i < 9) and tostring(i + 1) or "0",
-		mods = "CTRL",
-		action = wezterm.action.ActivateTab(i),
-	})
-end
 
 return keymaps
