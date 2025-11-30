efca6ea692934ec4709a0a46dc1ae28c825c0155
Jean Giraldo <jeann.giraldo@gmail.com>
2025-11-29 19:19:12 -0500

    refactor(neovim/ui): replace theme plugins with built-in `retrobox`

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,12 @@
-local COLORSCHEME = "tokyonight-moon"
-
 vim.g.mapleader = " "
 vim.o.exrc = true -- Load project-specific config using `.nvim.lua`
+
+vim.cmd("colorscheme retrobox")
 
 require("opts")
 require("keymaps")
 require("lsp")
 require("autocmds")
 require("plugins")
-
-vim.cmd("colorscheme " .. COLORSCHEME)
 
 require("ui")
