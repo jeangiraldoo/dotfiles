0d556f0cb9d653e56caf2e0ce355d72fb5d90be1
Jean Giraldo <jeann.giraldo@gmail.com>
2025-12-01 07:45:45 -0500

    chore(neovim): simplify `refactoring` plugin keymap descriptions

--- nvim/lua/plugins/intelligence.lua
+++ nvim/lua/plugins/intelligence.lua
@@ -53,25 +53,25 @@ return {
 			enabled = true,
 			keymaps = {
 				{
-					desc = "Extract occurrences of a selected expression to its own variable",
+					desc = "Extract variable",
 					mode = "x",
 					keys = "<leader>rv",
 					cmd = ":Refactor extract_var ",
 				},
 				{
-					desc = "Replace all occurrences of a variable with its value",
+					desc = "Inline variable",
 					mode = "n",
 					keys = "<leader>rV",
 					cmd = ":Refactor inline_var<CR>",
 				},
 				{
-					desc = "Extract code to a separate function",
+					desc = "Extract function",
 					mode = "x",
 					keys = "<leader>rf",
 					cmd = ":Refactor extract ",
 				},
 				{
-					desc = "Replace calls to the function declaration under the cursor with its body",
+					desc = "Inline function",
 					mode = "n",
 					keys = "<leader>rF",
 					cmd = ":Refactor inline_func<CR>",
