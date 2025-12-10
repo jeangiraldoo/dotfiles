716a0f393a71826361d4b5b9cbe69aa7c58f1278
jeangiraldoo <jeann.giraldo@gmail.com>
2025-12-10 11:57:27 -0500

    refactor(neovim/highlights): move statusline highlights into ´statusline.lua´

--- nvim/lua/ui/highlights.lua
+++ nvim/lua/ui/highlights.lua
@@ -4,24 +4,6 @@ utils.editor.set_highlights({
 	WhiteText = {
 		bg = "#ffffff",
 	},
-	StatusLineGitIcon = {
-		bg = "#b84500",
-		fg = "#d79921",
-	},
-	StatusLineGitText = {
-		fg = "#d79921",
-		bg = "#1e2030",
-	},
-	StatusLineGitContainer = {
-		bg = "#d79921",
-	},
-	StatusLinePositionText = {
-		bg = "#1e2030",
-		fg = "#d79921",
-	},
-	StatusLineLocationContainer = {
-		bg = "#d79921",
-	},
 	PMenu = {
 		bg = "#222222",
 		italic = true,
