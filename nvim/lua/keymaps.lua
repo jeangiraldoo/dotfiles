25c7503ace73eb41e9096c6c5bd75ee872645324
Jean Giraldo <jeann.giraldo@gmail.com>
2025-12-14 22:08:43 -0500

    chore(neovim/keymaps): replace window management keymaps with built-ins

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -133,19 +133,6 @@ local KEYMAPS = {
 		keys = "O",
 		cmd = "o<Esc>",
 	},
-	-- Window management
-	{
-		desc = "Increase vertical split",
-		mode = "n",
-		keys = "<C-Up>",
-		cmd = ":vertical resize +3 <CR>",
-	},
-	{
-		desc = "Decrease vertical split",
-		mode = "n",
-		keys = "<C-Down>",
-		cmd = ":vertical resize -3 <CR>",
-	},
 	{
 		desc = "Toggle casefile floating window",
 		mode = "n",
