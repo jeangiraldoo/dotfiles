d92c3a0f4e26ba0b23741ebe933373c7a14bf618
Jean Giraldo <jeann.giraldo@gmail.com>
2025-12-15 19:26:49 -0500

    refactor(neovim/utils): simplify ´fetch_join_tables´ for readability

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -4,38 +4,28 @@ local Utils = {
 	terminal = {},
 }
 
-local function _is_valid_lua_file(file_name)
-	local is_lua = file_name:sub(-4) == ".lua"
-	local is_init = file_name == "init.lua"
-	return is_lua and not is_init
-end
+---@param dir_name string
+---@return table[] acc Merged directory tables
+function Utils.module.require_config_tables(dir_name)
+	local full_path = vim.fs.joinpath(vim.fn.stdpath("config"), "lua", dir_name)
+	local dir_files = vim.fn.readdir(full_path, [[v:val =~ '\.lua$' && v:val !=# 'init.lua']])
 
-function Utils.module.fetch_join_tables(dir)
-	local full_path = vim.fn.stdpath("config") .. "/lua/" .. dir:gsub("%.", "/")
-	local dir_files = vim.fn.readdir(full_path)
+	return vim.iter(dir_files):fold({}, function(acc, file_name)
+		local lua_file_path = dir_name .. "." .. file_name:gsub("%.lua$", "")
 
-	local combined_entries = vim.iter(dir_files)
-		:filter(function(file_name)
-			return _is_valid_lua_file(file_name)
-		end)
-		:fold({}, function(acc, file_name)
-			local file_relative_path = dir .. "." .. file_name:gsub("%.lua$", "")
-
-			local ok, file_tbl = pcall(require, file_relative_path)
-			if not ok then
-				vim.notify("Failed to load " .. file_relative_path, vim.log.levels.WARN)
-				return acc
-			end
-
-			if vim.islist(file_tbl) then
-				return vim.list_extend(acc, file_tbl)
-			end
-
-			table.insert(acc, file_tbl)
+		local ok, file_tbl = pcall(require, lua_file_path)
+		if not ok then
+			vim.notify("Failed to load " .. lua_file_path, vim.log.levels.WARN)
 			return acc
-		end)
+		end
 
-	return combined_entries
+		if vim.islist(file_tbl) then
+			return vim.list_extend(acc, file_tbl)
+		end
+
+		table.insert(acc, file_tbl)
+		return acc
+	end)
 end
 
 function Utils.editor.set_highlights(highlights)
