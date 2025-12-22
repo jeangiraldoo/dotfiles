e0829b08095b005039f0890b7bac1591dc74340d
Jean Giraldo <jeann.giraldo@gmail.com>
2025-12-22 08:10:05 -0500

    refactor(install.sh): simplify script for reliability

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+declare -A packages
+packages[git]="latest"
+packages[gh]="latest"
+packages[zoxide]="latest"
+packages[lazygit]="latest"
+packages[yazi]="latest"
+packages[eza]="latest"
+packages[fish]="latest"
+packages[starship]="latest"
+
 declare -A HIGHLIGHTS
 HIGHLIGHTS[EXECUTION]="\033[35m" # Magenta
 HIGHLIGHTS[STATUS]="\033[34m" # Cyan
@@ -12,75 +22,45 @@ display_special_message() {
 
 display_special_message "Initializing setup...\n" EXECUTION
 
+declare -A MAIN_PKG_MANAGER_CMDS
+MAIN_PKG_MANAGER_CMDS[INSTALL]="sudo apt"
+MAIN_PKG_MANAGER_CMDS[UPDATE]="sudo apt update"
+
+declare -A SECONDARY_PKG_MANAGER_CMDS
+SECONDARY_PKG_MANAGER_CMDS[INSTALL]="brew"
+SECONDARY_PKG_MANAGER_CMDS[UPDATE]="brew update"
+
+# [Installing package managers]
+display_special_message "\nInstalling secondary package managers...\n" STATUS
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 # [Installing packages]
-display_special_message "Installing packages..." STATUS
+display_special_message "Installing packages..." EXECUTION
 
-declare -A packages
-packages[git]="latest"
-packages[gh]="latest"
-packages[zoxide]="latest"
-packages[lazygit]="latest"
-packages[yazi]="latest"
-packages[eza]="latest"
-packages[fish]="latest"
-packages[starship]="latest"
-
-declare -A OS_PKG_MANAGERS
-
-# Each package manager maps to its sudo requirement ("sudo" or "")
-OS_PKG_MANAGERS=(
-	[pkg]="" # For Termux
-    [apt]="sudo"
-    [dnf]="sudo"
-    [pacman]="sudo"
-    [zypper]="sudo"
-    [brew]=""
-)
-PREFERRED_ORDER=(pkg apt dnf pacman zypper brew)
-
-MAIN_PKG_MANAGER_NAME=""
-FALLBACK_PKG_MANAGER_NAME=""
-
-# Detect installed package managers in order of preference
-for mgr in "${PREFERRED_ORDER[@]}"; do
-    if command -v "$mgr" >/dev/null 2>&1; then
-        if [ -z "$MAIN_PKG_MANAGER_NAME" ]; then
-            MAIN_PKG_MANAGER_NAME="$mgr"
-        elif [ -z "$FALLBACK_PKG_MANAGER_NAME" ]; then
-            FALLBACK_PKG_MANAGER_NAME="$mgr"
-			break
-		fi
-	fi
-done
-
-if [ -z "$MAIN_PKG_MANAGER_NAME" ]; then
-	echo "No supported package manager found!"
-	exit 1
-fi
-
-MAIN_PKG_MANAGER_SUDO="${OS_PKG_MANAGERS[$MAIN_PKG_MANAGER_NAME]}"
-FALLBACK_PKG_MANAGER_SUDO="${OS_PKG_MANAGERS[$FALLBACK_PKG_MANAGER_NAME]}"
-
-display_special_message "Detected distro: $NAME
-Package managers:
-[$MAIN_PKG_MANAGER_SUDO] Main: $MAIN_PKG_MANAGER_NAME
-[${FALLBACK_PKG_MANAGER_SUDO}] Fallback: ${FALLBACK_PKG_MANAGER_NAME}" INFO
+update_cmd="${MAIN_PKG_MANAGER_CMDS[UPDATE]}"
+$update_cmd
 
 for app_name in "${!packages[@]}"; do
-	echo -e "\nInstalling $app_name"
+	display_special_message "\nInstalling $app_name" STATUS
 
-	cmd=($MAIN_PKG_MANAGER_SUDO $MAIN_PKG_MANAGER_NAME install $app_name)
-	if ! "${cmd[@]}"; then
-		fallback_cmd=($FALLBACK_PKG_MANAGER_SUDO $FALLBACK_PKG_MANAGER_NAME install $app_name)
-		echo "$MAIN_PKG_MANAGER_NAME failed. Attempting installation using $FALLBACK_PKG_MANAGER_NAME"
-		"${fallback_cmd[@]}"
+	ins_cmd="${MAIN_PKG_MANAGER_CMDS[INSTALL]} install $app_name"
+	if ! eval "$ins_cmd"; then
+		display_special_message "Installation failed with main package manager. Retrying with fallback..." INFO
+		fallback="${SECONDARY_PKG_MANAGER_CMDS[INSTALL]} install $app_name"
+		$fallback
 	fi
 done
 
-# [Cloning dotfiles]
-display_special_message "\nCloning dotfiles..." STATUS
+# [Install editor]
+display_special_message "Installing editor..." EXECUTION
 
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt install neovim
+
+# [Cloning dotfiles]
 DOTFILES_PATH="$HOME/.config"
+
+display_special_message "\nCloning dotfiles..." STATUS
 
 if [ ! -d "$DOTFILES_PATH" ] || [ -z "$(ls -A "$DOTFILES_PATH" 2>/dev/null)" ]; then
     echo "Cloning dotfiles into $DOTFILES_PATH..."
