1e7a25050c307ab7c21fe4b69b4a4deca1516c07
Jean Giraldo <jeann.giraldo@gmail.com>
2026-02-03 16:23:46 -0500

    chore(neovim/diagnostics): unset unnecessary options

--- nvim/lua/ui/init.lua
+++ nvim/lua/ui/init.lua
@@ -23,11 +23,8 @@ utils.editor.set_highlights({
 vim.diagnostic.config({
 	float = {
 		scope = "line",
-		border = "rounded",
-		source = "always",
 		header = "診断メッセージ",
 	},
 	signs = false,
-	underline = true,
 	severity_sort = true,
 })
