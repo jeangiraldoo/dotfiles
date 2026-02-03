1f97f3a1a2cbf133a4d6facb7bf267afdccead77
Jean Giraldo <jeann.giraldo@gmail.com>
2026-02-03 16:29:05 -0500

    refactor(neovim/ui): inline diagnostics config and highlights

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,6 +15,15 @@ vim.lsp.enable({
 	"yaml_ls",
 })
 
+vim.diagnostic.config({
+	float = {
+		scope = "line",
+		header = "診断メッセージ",
+	},
+	signs = false,
+	severity_sort = true,
+})
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
@@ -23,7 +32,7 @@ vim.opt.scroll = 10
 vim.opt.mouse = ""
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.statusline = "%!v:lua.require'ui.statusline'()"
+vim.opt.statusline = "%!v:lua.require'statusline'()"
 vim.opt.foldenable = true
 vim.opt.foldmethod = "manual"
 vim.opt.foldcolumn = "1"
@@ -38,8 +47,27 @@ vim.opt.pumborder = "rounded"
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
+require("utils").editor.set_highlights({
+	PMenuMatch = {
+		fg = "#ffffff",
+	},
+	PMenuMatchSel = {
+		fg = "#ffffff",
+	},
+	YankLine = {
+		bg = "#d79921",
+		fg = "#FFFFFF",
+	},
+	DiagnosticUnderlineError = {
+		underline = true,
+		fg = "#ff5555",
+	},
+	FloatBorder = {
+		fg = "#828100",
+	},
+})
+
 vim.cmd([[cnoreabbrev %% %:h]])
 require("keymaps")
 require("autocmds")
 require("plugins")
-require("ui")
