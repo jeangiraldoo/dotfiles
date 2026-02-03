e658775fcbd1a33bd369c629dfa012106f97d073
Jean Giraldo <jeann.giraldo@gmail.com>
2026-02-02 20:59:14 -0500

    chore(neovim/lsp): move LSP setup to `init` for simplicity

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,10 +3,20 @@ vim.o.exrc = true -- Load project-specific config using `.nvim.lua`
 
 vim.cmd("colorscheme retrobox")
 
+vim.lsp.log.set_level("off")
+vim.lsp.log.set_format_func(vim.inspect)
+vim.lsp.enable({
+	"lua_ls",
+	"rust_analyzer",
+	"basedpyright",
+	"harper_ls",
+	"marksman",
+	"tinymist",
+	"yaml_ls",
+})
+
 require("opts")
 require("keymaps")
-require("lsp")
 require("autocmds")
 require("plugins")
-
 require("ui")
