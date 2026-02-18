660fd1887d16094a838eb036a2be280811dac374
Jean Giraldo <jeann.giraldo@gmail.com>
2026-02-18 07:32:35 -0500

    chore(fish): remove unnecessary comments and options

--- fish/config.fish
+++ fish/config.fish
@@ -2,25 +2,19 @@ if not status is-interactive
 	return
 end
 
-# Environment variables
-set -Ux EDITOR nvim
-set -U fish_greeting ""
-set -g fish_key_bindings fish_vi_key_bindings
+fish_vi_key_bindings
 
-# Aliases
+set -U fish_greeting ""
+set -Ux EDITOR nvim
+
 alias G=lazygit
-alias e=$EDITOR
-alias E=vim
 alias ls="eza --all --icons=always"
 alias grep="grep --line-number --colour=always"
 
-# Abbreviations
 abbr dots "cd ~/.config"
 abbr sudo!! 'eval sudo $history[1]'
 
-# Vi mode
-fish_vi_key_bindings
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-# Initializations
 starship init fish | source
 zoxide init fish --cmd cd | source
