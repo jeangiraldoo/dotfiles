c2e28f285b2c649e20d77f3113c841ea4067b64b
Jean Giraldo <jeann.giraldo@gmail.com>
2026-03-03 08:56:18 -0500

    chore(neovim/GitSigns): overhaul default options and keymaps

--- nvim/lua/plugins/tools.lua
+++ nvim/lua/plugins/tools.lua
@@ -8,26 +8,24 @@ return {
 			enabled = true,
 			setup = function()
 				require("gitsigns").setup({
-					signcolumn = true,
 					current_line_blame_opts = {
 						delay = 0,
 					},
 					current_line_blame_formatter = "  <author> -> <summary> •  <author_time:%d-%b-%Y> • 󰜛 <abbrev_sha>",
 					current_line_blame_formatter_nc = " Not commited yet",
-					gh = true,
 				})
 			end,
 			keymaps = {
 				{
 					desc = "Toggle inline Git blame",
 					mode = "n",
-					keys = "<leader>pb",
+					keys = "<leader>gb",
 					cmd = ":Gitsigns toggle_current_line_blame<CR>",
 				},
 				{
 					desc = "Open buffer Git blame",
 					mode = "n",
-					keys = "<leader>pB",
+					keys = "<leader>gB",
 					cmd = ":Gitsigns blame<CR>",
 				},
 			},
