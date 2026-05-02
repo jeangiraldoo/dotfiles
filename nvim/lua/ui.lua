cf3b8a6e9d1e71bc4a3a061ea6ec220618fa80b6
Jean Giraldo <jeann.giraldo@gmail.com>
2026-05-02 16:21:08 -0500

    refactor(neovim/ui): remove custom statusline

--- nvim/lua/ui.lua
+++ nvim/lua/ui.lua
@@ -1,39 +1,9 @@
--- local utils = require "utils"
-
-function _G.build_statusline()
-	local RESET_HL = "%#StatusLine#"
-
-	local git_section = "%#statusline_section#█%#statusline_git_icon# "
-		.. "%#statusline_section_text#"
-		.. (vim.b.gitsigns_head or "[No Branch]")
-		.. "%#statusline_section#█"
-
-	local file_section = RESET_HL .. "%f" .. (vim.bo.modified and " %#WhiteText#●" or "")
-
-	local diagnostics_section = "%#DiagnosticWarn#☢" .. RESET_HL .. " " .. #vim.diagnostic.get(0)
-
-	local position_section = "%#statusline_section#"
-		.. "%#statusline_section_text#󰆌  %l:%c "
-		.. "%#statusline_section█"
-
-	return table.concat({
-		git_section,
-		file_section,
-		"%=",
-		diagnostics_section,
-		position_section,
-	}, " ")
-end
 
 vim.api.nvim_set_hl(0, "PMenuMatch", { fg = "#ffffff" })
 vim.api.nvim_set_hl(0, "PMenuMatchSel", { fg = "#ffffff" })
 vim.api.nvim_set_hl(0, "YankLine", { bg = "#d79921", fg = "#FFFFFF" })
 vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { underline = true, fg = "#ff5555" })
 vim.api.nvim_set_hl(0, "DiagnosticWarn", { bg = "orange" })
-vim.api.nvim_set_hl(0, "statusline_section_text", { bg = "#1e2030", fg = "#d79921" })
-vim.api.nvim_set_hl(0, "StatusLineGitText", { fg = "#d79921", bg = "#1e2030" })
-vim.api.nvim_set_hl(0, "statusline_git_icon", { fg = "#d79921", bg = "#b84500" })
-vim.api.nvim_set_hl(0, "statusline_section", { bg = "#d79921" })
 
 vim.api.nvim_create_autocmd("BufWinEnter", { desc = "Load view", pattern = "*", command = "silent! loadview" })
 vim.api.nvim_create_autocmd("BufWinLeave", { desc = "Make view", pattern = "*", command = "silent! mkview" })
